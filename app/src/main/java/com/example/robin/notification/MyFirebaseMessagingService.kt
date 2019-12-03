c7c8612e477878da7dccb4a8727d2c3e8baf88d3
Arttu Ylhävuori <10089872+areee@users.noreply.github.com>
2019-12-03 11:17:55 +0200

    updated the class
    - fixed all the error messages with the help of Android Studio
    - fixed the deprecated code

--- app/src/main/java/com/example/robin/notification/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/robin/notification/MyFirebaseMessagingService.kt
@@ -1,6 +1,5 @@
 package com.example.robin.notification
 
-import android.R
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -10,10 +9,9 @@ import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.media.RingtoneManager
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
-import com.google.firebase.messaging.FirebaseMessaging
+import androidx.core.content.ContextCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import java.util.*
@@ -24,11 +22,12 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     private val ADMIN_CHANNEL_ID = "admin_channel"
 
-    override fun onMessageReceived(p0: RemoteMessage?) {
+    override fun onMessageReceived(p0: RemoteMessage) {
         super.onMessageReceived(p0)
 
         val intent = Intent(this, MainActivity::class.java)
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         val notificationID = Random().nextInt(3000)
 
         /*
@@ -47,22 +46,23 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
         val largeIcon = BitmapFactory.decodeResource(
             resources,
-            R.drawable.ic_delete
+            android.R.drawable.ic_delete
         )
 
         val notificationSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_delete)
+            .setSmallIcon(android.R.drawable.ic_delete)
             .setLargeIcon(largeIcon)
-            .setContentTitle(p0?.data?.get("title"))
-            .setContentText(p0?.data?.get("message"))
+            .setContentTitle(p0.data["title"])
+            .setContentText(p0.data["message"])
             .setAutoCancel(true)
             .setSound(notificationSoundUri)
             .setContentIntent(pendingIntent)
 
         //Set notification color to match your app color template
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            notificationBuilder.color = resources.getColor(R.color.background_dark)
+            notificationBuilder.color =
+                ContextCompat.getColor(applicationContext, android.R.color.background_dark)
         }
         notificationManager.notify(notificationID, notificationBuilder.build())
     }
@@ -71,10 +71,14 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     @RequiresApi(api = Build.VERSION_CODES.O)
     private fun setupChannels(notificationManager: NotificationManager?) {
         val adminChannelName = "New notification"
-        val adminChannelDescription = "Device to devie notification"
+        val adminChannelDescription = "Device to device notification"
 
         val adminChannel: NotificationChannel
-        adminChannel = NotificationChannel(ADMIN_CHANNEL_ID, adminChannelName, NotificationManager.IMPORTANCE_HIGH)
+        adminChannel = NotificationChannel(
+            ADMIN_CHANNEL_ID,
+            adminChannelName,
+            NotificationManager.IMPORTANCE_HIGH
+        )
         adminChannel.description = adminChannelDescription
         adminChannel.enableLights(true)
         adminChannel.lightColor = Color.RED
